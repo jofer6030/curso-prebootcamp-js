8f8f6a056557012b3b81099fbe83d130cd1fafd5
jose fernando <jofer0628@gmail.com>
2022-08-05 11:49:43 -0500

    clase 1-2

---
+++ funciones.js
@@ -0,0 +1,28 @@
+// Hoisting
+saludar("Antes de declarar la funcion");
+
+function saludar(nombre = 'Desconocido') {
+  console.log("Hola",nombre);
+}
+
+saludar("Pedro");
+saludar("Juan");
+saludar();
+
+const sumar = function(num1 = 0,num2 = 0) {
+  console.log(num1 + num2);
+}
+
+sumar(4,5);
+sumar(10,5);
+sumar();
+
+// Arrow functions
+const restar = (num1 = 0,num2 = 0) => console.log(num1 - num2)
+const saludar2 = nombre => console.log(nombre)
+
+restar(10,5);
+restar(7,5);
+restar();
+
+saludar2("Pedro")
