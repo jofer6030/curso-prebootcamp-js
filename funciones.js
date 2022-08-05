909b88bd76f0f13df44852becf4006a68899538d
jose fernando <jofer0628@gmail.com>
2022-08-05 11:56:32 -0500

    clase 1-2 return added

--- funciones.js
+++ funciones.js
@@ -18,11 +18,18 @@ sumar(10,5);
 sumar();
 
 // Arrow functions
-const restar = (num1 = 0,num2 = 0) => console.log(num1 - num2)
+const restar = (num1 = 0, num2 = 0) => console.log(num1 - num2)
 const saludar2 = nombre => console.log(nombre)
 
 restar(10,5);
 restar(7,5);
 restar();
 
-saludar2("Pedro")
+// saludar2("Pedro")
+
+const multiplicar = (num1 = 0, num2 = 0) => {
+  return num1 * num2;
+}
+
+const multiplicacion = multiplicar(10,5);
+console.log(multiplicacion);
